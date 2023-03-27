446785031c8f469b53a678f1bf1214bd4d9bcfc9
David Giraldo <degiraldod@gmail.com>
2023-03-27 16:13:33 -0500

    menu2 integrated

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const emailMenu = document.querySelector(".account")
 const desktopMenu = document.querySelector(".desktop-menu")
+const mobileMenu = document.querySelector(".mobile-menu")
+const iconMenu = document.querySelector(".menu")
 
 emailMenu.addEventListener("click", toggleDesktopMenu)
+iconMenu.addEventListener("click", toggleMobileMenu)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive")
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive")
 }
