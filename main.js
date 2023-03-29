b15ff5176995dcf9497736cdff82d05dceb13223
David Giraldo <degiraldod@gmail.com>
2023-03-28 19:39:02 -0500

    shopping-cart integrated

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const emailMenu = document.querySelector(".account")
 const desktopMenu = document.querySelector(".desktop-menu")
 const mobileMenu = document.querySelector(".mobile-menu")
 const iconMenu = document.querySelector(".menu")
+const iconCart = document.querySelector(".shopping-cart-icon")
+const shoppingCart = document.querySelector(".shopping-cart")
 
 emailMenu.addEventListener("click", toggleDesktopMenu)
 iconMenu.addEventListener("click", toggleMobileMenu)
+iconCart.addEventListener("click", toggleShoppingCart)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive")
@@ -12,4 +15,11 @@ function toggleDesktopMenu() {
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle("inactive")
+    shoppingCart.classList.add("inactive")
+}
+
+function toggleShoppingCart() {
+    shoppingCart.classList.toggle("inactive")
+    mobileMenu.classList.add("inactive")
+    desktopMenu.classList.add("inactive")
 }
